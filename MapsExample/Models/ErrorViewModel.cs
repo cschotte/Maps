c2b6ec9b935cb16027cf795b986323a3c4b8641e
Clemens Schotte <clemens@navatron.com>
2022-08-08 21:00:29 +0200

    Add Authorization

---
+++ MapsExample/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MapsExample.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
