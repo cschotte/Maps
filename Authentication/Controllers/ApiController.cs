ac884542f77dfc6853204e970e4b57ceb5bad713
Clemens Schotte <clemens@navatron.com>
2022-08-09 11:07:08 +0200

    token provider

--- Authentication/Controllers/ApiController.cs
+++ Authentication/Controllers/ApiController.cs
@@ -8,8 +8,19 @@ namespace MapsExample.Controllers
     [Authorize]
     public class ApiController : Controller
     {
-        private readonly string[] allowed = { "https://navatron-maps.azurewebsites.net/",
-                                              "https://localhost"};
+        /// <summary>
+        /// This token provider simplifies access tokens for Azure Resources. It uses the Managed Identity of the deployed resource.
+        /// For instance if this application was deployed to Azure App Service or Azure Virtual Machine, you can assign an Azure AD
+        /// identity and this library will use that identity when deployed to production.
+        /// </summary>
+        /// <remarks>
+        /// This tokenProvider will cache the token in memory, if you would like to reduce the dependency on Azure AD we recommend
+        /// implementing a distributed cache combined with using the other methods available on tokenProvider.
+        /// </remarks>
+        private static readonly DefaultAzureCredential tokenProvider = new();
+
+        private static readonly string[] allowed = { "https://navatron-maps.azurewebsites.net/",
+                                                     "https://localhost"};
 
         public async Task<IActionResult> GetAzureMapsToken()
         {
@@ -24,8 +35,7 @@ namespace MapsExample.Controllers
             // Managed identities for Azure resources and Azure Maps
             // For the Web SDK to authorize correctly, you still must assign Azure role based access control for the managed identity
             // https://docs.microsoft.com/en-us/azure/azure-maps/how-to-manage-authentication
-            var tokenCredential = new DefaultAzureCredential();
-            var accessToken = await tokenCredential.GetTokenAsync(
+            var accessToken = await tokenProvider.GetTokenAsync(
                 new TokenRequestContext(new[] { "https://atlas.microsoft.com/.default" })
             );
 
